c64195200c05e4dbd49172e537bdd51a5767cc6d
kliment.t <kliment.t@hotmail.com>
2023-06-12 17:38:26 +0200

    home page small changes

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-
 /* Homepage SCRIPT 🡻 */
 
-/* code for homepage here (delete this line)*/
-
 /* End of Homepage SCRIPT 🡹  */
-
-
 
 /* Navigation bar SCRIPT 🡻 */
 
@@ -13,18 +8,11 @@
 
 /* Navigation bar SCRIPT 🡹  */
 
-
-
-
-
 /* Custom burger SCRIPT 🡻 */
 
 /* code for custom burger here  (delete this line)*/
 
 /* End of custom burger SCRIPT 🡹  */
-
-
-
 
 /* About us SCRIPT 🡻 */
 
@@ -32,18 +20,11 @@
 
 /* End of about us SCRIPT 🡹  */
 
-
-
-
 /* Contact SCRIPT 🡻 */
 
 /* code for contact here  (delete this line)*/
 
 /* End of contact SCRIPT 🡹  */
-
-
-
-
 
 /* History  SCRIPT 🡻 */
 
@@ -51,20 +32,11 @@
 
 /* End of history SCRIPT 🡹  */
 
-
-
-
-
 /* Cart SCRIPT 🡻 */
 
 /* code for cart here  (delete this line)*/
 
 /* End of cart  SCRIPT 🡹  */
-
-
-
-
-
 
 /* Rewiew  SCRIPT 🡻 */
 
@@ -72,18 +44,11 @@
 
 /* End of custom burger SCRIPT 🡹  */
 
-
-
-
-
 /* Menu SCRIPT 🡻 */
 
 /* code for menu here  (delete this line)*/
 
 /* End of menu SCRIPT 🡹  */
 
-
-
-
-// Login ovde proba neso 
+// Login ovde proba neso
 // Check if the user is already logged in
