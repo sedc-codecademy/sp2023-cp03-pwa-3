d4094721c529ba4ac5cb47136633d1d6b2c9ca44
Strezo <kokizverot@hotmail.com>
2023-06-13 12:01:04 +0200

    Contact page

---
+++ Contact/script.js
@@ -0,0 +1,24 @@
+const menuIcon = document.querySelector('.Menue')
+const navbar=document.querySelector('.navbar')
+
+menuIcon.addEventListener('click',()=>{
+navbar.classList.toggle('change');
+})
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
