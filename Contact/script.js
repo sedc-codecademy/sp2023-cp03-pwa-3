0915b0e731922aff437d8646830253028964e727
Strezo <kosta_oh@hotmail.com>
2023-06-24 15:45:32 +0200

    Contact done

--- Contact/script.js
+++ Contact/script.js
@@ -1,8 +1,14 @@
 const menuIcon = document.querySelector('.Menue')
 const navbar=document.querySelector('.navbar')
+const blurOverlay = document.querySelector('.blur');
 
 menuIcon.addEventListener('click',()=>{
 navbar.classList.toggle('change');
+if (blurOverlay.style.backdropFilter === 'blur(10px)') {
+  blurOverlay.style.backdropFilter = 'none';
+} else {
+  blurOverlay.style.backdropFilter = 'blur(10px)';
+}
 })
 const inputs = document.querySelectorAll(".input");
 
