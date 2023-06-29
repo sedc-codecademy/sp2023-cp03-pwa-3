48442b6a4715d59df14fd80c451652f00983a9fa
NikolaDostin <nikoladostin1@gmail.com>
2023-06-29 15:00:12 +0200

    Mali izmeni , like button se dodava dinamicno , 5 star rating otstranet

--- ReviewsPage/script.js
+++ ReviewsPage/script.js
@@ -35,8 +35,6 @@ document.getElementById('logout-button').addEventListener('click', function() {
 // Check the login status when the page loads
 checkLoginStatus();
 
-
-//ovde za submit i ostalo sranje da se vagjat dinamicno so si sveeee 
 // Handle the submit review button click
 document.getElementById('submit-review').addEventListener('click', function() {
   var title = document.getElementById('title').value;
@@ -65,37 +63,34 @@ document.getElementById('submit-review').addEventListener('click', function() {
     reviewText.textContent = review;
 
     var likeButton = document.createElement('button');
-likeButton.classList.add('like-button');
-likeButton.textContent = 'Like';
-
-
-
-var counter = 0; // Initial counter value
-var counterElement = document.createElement('span');
-counterElement.classList.add('counter');
-counterElement.textContent = counter;
-
-likeButton.addEventListener('click', function() {
-  if (likeButton.classList.contains('liked')) {
-    // Decrease counter by 1
-    counter--;
-    counterElement.textContent = counter;
-    likeButton.classList.remove('liked');
+    likeButton.classList.add('like-button');
     likeButton.textContent = 'Like';
-  } else {
-    // Increase counter by 1
-    counter++;
+
+    var counter = 0; // Initial counter value
+    var counterElement = document.createElement('span');
+    counterElement.classList.add('counter');
     counterElement.textContent = counter;
-    likeButton.classList.add('liked');
-    likeButton.textContent = 'Liked';
-  }
-});
 
-var likeButtonContainer = document.createElement('div');
-likeButtonContainer.classList.add('like-button-container');
-likeButtonContainer.appendChild(likeButton);
-likeButtonContainer.appendChild(counterElement);
+    likeButton.addEventListener('click', function() {
+      if (likeButton.classList.contains('liked')) {
+        // Decrease counter by 1
+        counter--;
+        counterElement.textContent = counter;
+        likeButton.classList.remove('liked');
+        likeButton.textContent = 'Like';
+      } else {
+        // Increase counter by 1
+        counter++;
+        counterElement.textContent = counter;
+        likeButton.classList.add('liked');
+        likeButton.textContent = 'Liked';
+      }
+    });
 
+    var likeButtonContainer = document.createElement('div');
+    likeButtonContainer.classList.add('like-button-container');
+    likeButtonContainer.appendChild(likeButton);
+    likeButtonContainer.appendChild(counterElement);
 
     var deleteButton = document.createElement('button');
     deleteButton.classList.add('delete-button');
@@ -113,7 +108,7 @@ likeButtonContainer.appendChild(counterElement);
     reviewHeader.appendChild(reviewTitle);
     reviewHeader.appendChild(usernameElement);
     reviewBody.appendChild(reviewText);
-    reviewBody.appendChild(likeButton);
+    reviewBody.appendChild(likeButtonContainer);
     reviewBody.appendChild(deleteButton);
     reviewItem.appendChild(reviewHeader);
     reviewItem.appendChild(reviewBody);
@@ -127,7 +122,7 @@ likeButtonContainer.appendChild(counterElement);
     }
 
     // Scroll to the latest review item
-    reviewsList.scrollTop = reviewsList.scrollHeight;
+    reviewItem.scrollIntoView({ behavior: 'smooth', block: 'end' });
 
     // Clear the form fields
     document.getElementById('title').value = '';
@@ -139,52 +134,3 @@ likeButtonContainer.appendChild(counterElement);
     }, 10);
   }
 });
-
-
-//ovde submit button like edit i sve 
-// Handle the like functionality
-var likeButtons = document.querySelectorAll('.like-button');
-likeButtons.forEach(function(likeButton) {
-  likeButton.addEventListener('click', function() {
-    if (likeButton.textContent === 'Like') {
-      likeButton.textContent = 'Liked';
-      // Perform any additional like-related functionality
-    } else {
-      likeButton.textContent = 'Like';
-      // Perform any additional unlike-related functionality
-    }
-  });
-});
-
-var counterValue = 0; // Initial counter value
-var counterElement = document.getElementById('counter');
-var likeButton = document.getElementById('likeButton');
-
-likeButton.addEventListener('click', function() {
-  if (likeButton.classList.contains('liked')) {
-    // Decrease counter by 1 and update the display
-    counterValue--;
-    counterElement.textContent = counterValue;
-    likeButton.classList.remove('liked');
-  } else {
-    // Increase counter by 1 and update the display
-    counterValue++;
-    counterElement.textContent = counterValue;
-    likeButton.classList.add('liked');
-  }
-});
-
-// Handle the delete functionality
-var deleteButtons = document.querySelectorAll('.delete-button');
-deleteButtons.forEach(function(deleteButton) {
-  deleteButton.addEventListener('click', function() {
-    var reviewItem = deleteButton.closest('.review');
-    var confirmDelete = confirm('Are you sure you want to delete this review?');
-    if (confirmDelete) {
-      reviewItem.remove();
-      // Perform any additional delete-related functionality
-    }
-  });
-});
-
-
